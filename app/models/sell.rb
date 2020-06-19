bf9bbb88b5d9c5a11d828a09b0a67ac5b5b3fc4f
José Luiz Junior <junior.jr.jl@gmail.com>
2020-06-19 16:22:18 -0300

    ajustes de menu, configuraçao da relaçao de vendas com produtos e servicos, criaçao do mailer

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -2,12 +2,17 @@ class Sell < ApplicationRecord
   include Fae::BaseModelConcern
   validates :client, presence: true
   enum status: { finished:0, canceled: 1}
+
+  belongs_to :discount
+  belongs_to :client
   
   has_many :sell_products
-  has_many :products, through: :sell_products
+  has_many :products, through: :sell_products, dependent: :destroy
   
   has_many :sell_services
-  has_many :services, through: :sell_services
+  has_many :services, through: :sell_services, dependent: :destroy
+
+  before_save :set_total
   
   def fae_display_field
     id
@@ -17,6 +22,18 @@ class Sell < ApplicationRecord
     order(:id)
   end
 
-  belongs_to :discount
-  belongs_to :client
+  private
+
+  def set_total
+    total = 0
+    self.products.each{|p| total += p.price}
+    self.services.each{|s| total += s.price}
+    
+    if self.discount.present?
+      total -= self.discount.value
+    end
+    total = (total >= 0) ? total : 0
+    self.total = total
+  end
+
 end
